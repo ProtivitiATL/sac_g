8349e51fe55aed595dce8045576a41863f2d9b76
ProtivitiATL <58958220+ProtivitiATL@users.noreply.github.com>
2020-12-08 18:26:33 -0500

    Add files via upload

--- public/pGauge.js
+++ public/pGauge.js
@@ -327,7 +327,7 @@ Matt Gauge JS
     p = y = b.minValue;
     if (!b.renderTo) throw Error("Canvas element was not specified when creating the Gauge object!");
     var z = b.renderTo.tagName ? b.renderTo : document.getElementById(b.renderTo),
-        a = z.getContext("2d"),
+        a = document.getElementById(b.renderTo).getContext("2d"),
         A,
         C,
         D,
